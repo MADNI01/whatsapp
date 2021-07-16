c86998c463ff6ea50cf1c165c798b9d5d7080343
MADNI01 <79811973+MADNI01@users.noreply.github.com>
2021-07-16 18:37:46 +0300

    change loading path

--- components/Loading.js
+++ components/Loading.js
@@ -1,5 +1,5 @@
 import {Circle} from 'better-react-spinkit'
-import classes from './loading.module.css'
+import classes from './Loading.module.css'
 const Loading = () => {
     return ( 
         <div className={classes.loading}>
